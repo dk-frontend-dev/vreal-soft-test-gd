9b04e2b6bd0a7fda0bc5f80c0bb7370beee84e98
Davyd Kasumov <davidkasumovfrontend@gmail.com>
2023-11-30 19:28:08 +0200

    fix: removed autocomplete from appSearchField

--- packages/website/src/shared/ui/AppSearchField/AppSearchField.tsx
+++ packages/website/src/shared/ui/AppSearchField/AppSearchField.tsx
@@ -27,7 +27,7 @@ function AppSearchField({onValueChange}: AppSearchFieldProps) {
 
     return (
         <div className={s.container}>
-            <TextField aria-autocomplete={'none'} placeholder={'Search by name..'} onChange={onChange} value={value} />
+            <TextField autoComplete='off' type='text' aria-autocomplete={'none'} placeholder={'Search by name..'} onChange={onChange} value={value} />
             <AppButton text={'Reset'} size={'large'} onClick={clearField} />
         </div>
     )
